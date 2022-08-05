4be2b94573f5cb8ae01f87f49df73b8cd626f5ad
RocketeerEatingEggs <94308822+RocketeerEatingEggs@users.noreply.github.com>
2022-08-05 12:37:59 -0400

    Upload command line DeIFFMOD app

---
+++ DeIFFMOD.py
@@ -0,0 +1,31 @@
+import sys
+blankString = ""
+openedFilename = sys.argv[1]
+if openedFilename != "":
+    newFilename = sys.argv[2]
+    with open(openedFilename, "rb") as openedFile:
+        with open(newFilename, "wb") as newModFile:
+            openedFile.seek(8)
+            magic = str(openedFile.read(4), encoding="iso-8859-1")
+            if magic == "MODL":
+                if str(openedFile.read(4), encoding="iso-8859-1") == "VERS":
+                    openedFile.seek(14, 1)
+                    if str(openedFile.read(4), encoding="iso-8859-1") == "INFO":
+                        openedFile.seek(68, 1)
+                        if str(openedFile.read(4), encoding="iso-8859-1") == "CMNT":
+                            lenCMNT = int.from_bytes(openedFile.read(4), byteorder='big')
+                            openedFile.seek(lenCMNT - 8, 1)
+                            if str(openedFile.read(4), encoding="iso-8859-1") == "PTDT":
+                                openedFile.seek(4, 1)
+                                newModFile.write(openedFile.read())
+                                print('The information has been stripped from your file.')
+                            else:
+                                print('PTDT')
+                        else:
+                            print('CMNT')
+                    else:
+                        print('INFO')
+                else:
+                    print('VERS')
+            else:
+                print('MODL')
